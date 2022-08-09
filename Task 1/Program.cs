da65a19cf9a284cf552cca4b6024d8187521c9a4
Савелий <Savka9765@yandex.ru>
2022-08-09 20:42:30 +0300

    Task 1. Программа, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.

---
+++ Task 1/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+Console.Write("Введите целое трехзначное число: ");
+int user_number = Convert.ToInt32(Console.ReadLine());
+
+int second_number(int user_number)
+{
+   int result = user_number / 10 % 10;
+   return result; 
+}
+
+int res = second_number(user_number);
+Console.WriteLine(res);
